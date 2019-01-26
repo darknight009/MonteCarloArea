055b80c4a37f8ed3f52c28cc1c27e299c2622e6b
Bhaavan Goel <cbhaavan@gmail.com>
2019-01-26 18:01:03 +0530

    Lies Within and thinning
    Introduces:
    > Check whether a point lies within a closed curve
    > Primitive thinning to remove extra pixels from the boundary
    >Monte carlo simulation

--- main.py
+++ main.py
@@ -1,76 +1,104 @@
-# import tkinter as tk
-# root = tk.Tk()
-# canvas_1 = tk.Canvas(root, width=250, height=200)
-# canvas_1.grid(row=0, column=1)
-
-# point_dicx={}
-# point_dicy={}
-
-# def mmove(event):
-#    python_green = "#476042"
-#    x1, y1 = (event.x - 1), (event.y - 1)
-#    x2, y2 = (event.x + 1), (event.y + 1)
-#    canvas_1.create_oval(x1, y1, x2, y2, fill=python_green)
-#    point_dicx[event.x]=event.y
-#    point_dicy[event.y]=event.x
-
-# root.bind('<Motion>', mmove)
-# root.mainloop()
-# print(point_dicx)
-# print(point_dicy)
-
+import cv2
+import numpy
 import random
 
-min_x = 0
-max_x = 80
-min_y = 0
-max_y = 60
+def liesWithin(y, x, image, width):
+   inx=0
+   prev=0
+   for i in range(x, width):
+      if image[y][i][0]==0 and image[y][prev][0]!=0:
+         inx+=1
+      prev=i
+   return inx
 
-up=[]
-for x in range(60):
-      up.append([x, 0])
+image = cv2.imread("rect.png")
+original=cv2.imread("rect.png")
+# kernel = numpy.ones((3,3),numpy.uint8)
+# eroded = cv2.erode(image,kernel,iterations = 1)
+# eroded = cv2.erode(eroded,kernel,iterations = 1)
 
-down=[]
-for x in range(60):
-      down.append([x, 40])
+# cv2.imshow('Main', image)
+# cv2.imshow('Numpy Vertical', eroded)
+# cv2.waitKey(0)
 
-right=[]
-for y in range(40):
-      right.append([60, y])
+height, width, channels = image.shape
+redundant=[[0 for i in range(width)] for j in range(height)]
+# for it in range(1):
+#    for j in range(1, height-1):
+#       for i in range(1, width-1):
+#          kernel=[]
+#          kernel.append(image[j][i-1]);
+#          kernel.append(image[j+1][i-1]);
+#          kernel.append(image[j+1][i]);
+#          kernel.append(image[j+1][i+1]);
+#          kernel.append(image[j][i+1]);
+#          kernel.append(image[j-1][i+1]);
+#          kernel.append(image[j-1][i]);
+#          kernel.append(image[j-1][i-1]);
+#          kernel.append(image[j][i-1]);
+#          sp=0
+#          np=0
+#          for k in range(8):
+#             if not numpy.array_equal(kernel[k+1], kernel[k]):
+#                sp+=1
+#             if kernel[k].all()==0:
+#                np+=1
 
-left=[]
-for y in range(40):
-      left.append([0, y])
+#          if np in [0, 1, 7, 8] or sp<2:
+#             redundant[j][i]=1
 
-count=0
+#    for j in range(1, height-1):
+#       for i in range(1, width-1):
+#          if redundant[j][i]!=1:
+#             image[j][i]=[255, 255, 255]
+
 count_in=0
-
-for i in range(1000000):
-   x=random.randint(1, max_x)
-   y=random.randint(1, max_y)
-   if x<60:
-      if y<40:
-         count_in+=1
+count=0
+for r in range(10000):
+   x=random.randint(0, width-1)
+   y=random.randint(0, height-1)
+   lw=liesWithin(y, x, image, width)
+   if lw%2==1 and image[y][x][0]!=0:
+      count_in+=1
+      original[y][x] = [0, 0, 255]
+      if lw!=1:
+         print(y,x,lw)
+         original[y][x] = [0, 255, 0]
    count+=1
 
-print(count, count_in)
-print(80*60*count_in/count)
 
-# from Tkinter import *
-# #from tkinter import * # For Python 3.2.3 and higher.
-# root = Tk()
-# root.title(‘Smoothed line’)
-# cw = 250 # canvas width
-# ch = 200 # canvas height
-# canvas_1 = Canvas(root, width=cw, height=ch, background=”pink”)
-# canvas_1.grid(row=0, column=1)
+ratio=count_in/count
+outer_area=width*height
+inner_area=ratio*outer_area
+print(inner_area, outer_area, ratio)
 
-# x1 = 50
-# y1 = 10
-# x2 = 50
-# y2 = 180
-# x3 = 180
-# y3 = 180
+cv2.imshow('original', original)
+cv2.imshow('thinned', image)
+cv2.waitKey(0)
 
-# canvas_1.create_line(x1,y1, x2,y2, x3,y3, smooth=”true”)
-# root.mainloop()
+
+# print(boundary)
+# boundary = smooth(boundary)
+# print(boundary)
+
+# img = numpy.zeros((height,width,3), numpy.uint8)
+
+# for x in boundary:
+#    if len(boundary[x])>1:
+#       cv2.rect(img,(x,boundary[x][0]), 1, (0,0,255), -1)
+#       cv2.rect(img,(x,boundary[x][1]), 1, (0,0,255), -1)
+#    else:
+#       print(x, boundary[x])
+#       cv2.rect(img,(x,boundary[x][0]), 1, (0,0,255), -1)
+
+# # cv2.imshow('Draw01',img)
+# # cv2.waitKey(0)
+
+# count=0
+
+# for i in range(10000):
+#    y = random.random() * height
+#    x = random.random() * width
+#    count+=liesWithin(boundary, x, y)
+
+# print(count)
